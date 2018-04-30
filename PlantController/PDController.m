7fe05849eba8ad159417b7544d7f62b2e5cac8c3
User Name <krithika@g.harvard.edu>
2018-04-30 14:53:39 -0700

    Adding updated files for running with RL framework

--- PlantController/PDController.m
+++ PlantController/PDController.m
@@ -10,42 +10,59 @@ classdef PDController < DrakeSystem
     q_num
     qdot_num
     use_lqr % set to false as it is not linearizable
-    in_rl % in training?
     in_pb % in playback mode?
-    xtraj
-    utraj
+    xtraj % example trajectory used for time indexing
+    utraj % controller outputs to be commanded
   end
 
   methods
     function obj = PDController(plant,Kp,Kd,cv,cd,xtraj,utraj)
-%     function obj = PDController(plant,Kp,Kd,cv,cd)
+        
+
         global current_target_state
+        
         obj = obj@DrakeSystem(0,0,14,4,true,false);
         obj.p = plant;
-%         obj.qdes = obj.getState(current_target_state);
         obj.use_lqr = 0; % use for testing lqr gains - dlinearize gives error that the system is not controllable
+        
+        % Set up PD controller gains
         [obj.Kp, obj.Kd] = obj.calcK(Kp,Kd);
         obj.cv = cv;
         obj.cd = cd;
+        
+        
         obj = obj.setInputFrame(plant.getStateFrame);
         obj = obj.setOutputFrame(plant.getInputFrame);
+        
+        % Set size of state space
         obj.q_num = getNumPositions(obj.p);
         obj.qdot_num = getNumVelocities(obj.p);
-        obj.in_rl = 0;
-        obj.in_pb = 0;
-        obj.xtraj = xtraj;
-        obj.utraj = utraj;
+        
+        obj.in_pb = 1; % in playback?
+        if exist('xtraj', 'var')
+            obj.xtraj = xtraj;
+        else
+            obj.in_pb = 0; % not in playback
+        end
+        
+        if exist('utraj', 'var')
+            obj.utraj = utraj;
+        else
+            obj.in_pb = 0; % not in playback
+        end
+
+        
         global state_targets;
     end
     
     function s = getState(obj,stateind,x)
+        % Sets the target pose given the current state and mode
         global state_targets
+        
+        % Adjust state to control for instability in the torso
         s_new = obj.feedback_adjust_state(stateind, x);
+        
         s = Point(getStateFrame(obj.p));
-%         s.left_upper_leg_pin = state_targets{stateind}(1);
-%         s.right_upper_leg_pin = state_targets{stateind}(4);
-%         s.left_knee_pin = state_targets{stateind}(2);
-%         s.right_knee_pin = state_targets{stateind}(5);
         s.left_upper_leg_pin = s_new(1);
         s.right_upper_leg_pin = s_new(4);
         s.left_knee_pin = s_new(2);
@@ -54,6 +71,8 @@ classdef PDController < DrakeSystem
     end
     
     function y = updateState(obj,t,old_y,x)
+      % Checks to see if the mode needs to be updated and returns the
+      % updated state and time of last update
       global last_update_time
 
       [left_h, left_x] = obj.left_foot_coords(x);
@@ -64,13 +83,13 @@ classdef PDController < DrakeSystem
       update_interval = 0.4;
       
       time_up = t - last_update_time > update_interval;
-%       max_swing_dist = 0.8;
+
       if state == 1 || state == 3
-          should_update = time_up; %| abs(left_x - right_x) > max_swing_dist;
+          should_update = time_up;
       elseif state == 2
-          should_update = (left_h < 0.0005);
+          should_update = (left_h < 0.005);
       else
-          should_update = (right_h < 0.0005);
+          should_update = (right_h < 0.005);
       end
       
       if should_update
@@ -89,61 +108,48 @@ classdef PDController < DrakeSystem
     end
 
     function u = output(obj,t,y,x)
+        % Calculates the controller output
         
-        [left_h, left_x] = obj.left_foot_coords(x);
-        left_contact = left_h < 0.005;
-        [right_h, right_x] = obj.right_foot_coords(x);
-        right_contact = right_h < 0.005;
-        
-%         global stance_time
         global current_target_state
         global last_update_time
         
-%         if (current_target_state == 1 || current_target_state == 2)
-%             if left_contact && (t-stance_time) > 0.1
-%                 stance_time = t;
-%             end
-%         else
-%             if right_contact && (t-stance_time) > 0.1
-%                 stance_time = t;
-%             end
-%         end
-        
+        % Update target pose if necessary
         y = obj.updateState(t,[current_target_state; last_update_time],x);
         current_target_state = y(1);
         
+        % 
         obj.qdes = obj.getState(current_target_state,x);
         
-        
+        % PD controller output
         u = -obj.Kp*(x(1:obj.q_num)-obj.qdes(1:obj.q_num))-obj.Kd*x(obj.q_num+1:obj.q_num+obj.qdot_num);
         
-%         if obj.in_rl
-%             u_rl = get_u();
-%             u = u + u_rl;
-%         end
-        
+        % if we are in playback mode, command torques from the utraj provided
         if obj.in_pb
             t_ind = find(obj.xtraj.tt == t);
             u = obj.utraj(:,t_ind);
         end
         
+        % Threshold controller outputs - these limits were chosen somewhat randomly
         u = min(u,200);
         u = max(u,-200);
         
     end
     
     function [Kp_full,Kd] = calcK(obj,Kp,Kd)
+        % Calculates gain matrices to use in PD controller
         if(obj.use_lqr)
             [A,B,C,D,xn0,y0] = obj.p.dlinearize(0.001,0,double(obj.qdes),0);
             Q = 1000*eye(size(A));
             R = eye(size(B,2));
             Kp = lqr(A,B,Q,R);
+            
         else
             Kp_full = Kp*([ 0 0 0 1 0 0 0;...
                 0 0 0 0 1 0 0;...
                 0 0 0 0 0 1 0;...
                 0 0 0 0 0 0 1]); % Only apply P control on positions and D control on velocities
             Kd = Kd/Kp*Kp_full;
+            
         end
     end
     
@@ -181,23 +187,29 @@ classdef PDController < DrakeSystem
     end
     
     function state = feedback_adjust_state(obj, ind, x)
+      % Adjust target pose to account for torso instabilities
+      
       global state_targets;
+      
       v = x(8);
-%       c_v = 0.2;
-%       c_d = 2.2;
-
       state = state_targets{ind};
+      
       if ind == 1
         state(1) = state(1) - obj.cv*v;
+        
       elseif ind == 3
         state(4) = state(4) - obj.cv*v;
+        
       elseif ind == 2
         [right_h, right_x] = obj.right_foot_coords(x);
         state(1) = state(1) - obj.cd * right_x;
+        
       elseif ind == 4
         [left_h, left_x] = obj.left_foot_coords(x);
         state(4) = state(4) - obj.cd * left_x;
+        
       end
+      
     end
     
   end
--- PlantController/runPDx.m
+++ PlantController/runPDx.m
@@ -1,6 +1,6 @@
 % Setup KneedCompassGait plant
-tic
-% function zf = runPDx(Kp,Kd,cv,cd,r)
+
+% Set up plant
 options = [];
 options.floating = true;
 options.terrain = RigidBodyFlatTerrain();
@@ -9,23 +9,20 @@ options.use_bullet = false;
 m = PlanarRigidBodyManipulator('KneedCompassGait_noankles.urdf', options);
 r = TimeSteppingRigidBodyManipulator(m,.001);
 
-% Setup visualizer
+% Setup visualizer - comment out for speed
 % v = r.constructVisualizer;
 % v.axis = [-1.7 5.2 -0.1 1.6];
 % v.display_dt = .05;
 
 % Set up global variables
-% global state;
 global state_targets;
 global current_target_state;
-global stance_time;
 global last_update_time;
 
-current_target_state = 1; % starting state
-stance_time = 0;
+current_target_state = 3; % starting state - can only be 1 or 3 for realistic starting poses
 last_update_time = 0;
 
-torso_lean = 0.1;
+torso_lean = 0.025;
 max_hip_angle = 1.4;
 max_knee_angle = 0.6;
 leg_cross = 1.2;
@@ -40,6 +37,21 @@ state_targets = {
         [0; straight_knee; bend_ankle; -leg_cross/2 - torso_lean; max_knee_angle; bend_ankle],... % right bend
         [0; straight_knee; kick_ankle; max_hip_angle/2 - torso_lean; straight_knee; kick_ankle],... % right kick back
       };
+  
+% construct PD controller - note that with the current set up, these
+% constants are not used and thus have no effect
+Kp = 170;
+Kd = 2*sqrt(Kp);
+cv = 0.1;
+cd = 0;
+  
+xtraj = runPD(Kp,Kd,cv,cd,r,xtraj2,utraj2);
+
+function xtraj = runPD(Kp,Kd,cv,cd,r,xtraj,utraj)
+
+global state_targets;
+global current_target_state;
+global last_update_time;
 
 % Set initial condition
 x0 = Point(r.getStateFrame());
@@ -51,31 +63,34 @@ x0.right_knee_pin = state_targets{current_target_state}(5);
 
 % Update state
 current_target_state = mod(current_target_state + 1,4);
-% if current_target_state == 0
-%     current_target_state = 4;
-% end
+if current_target_state == 0
+    current_target_state = 4;
+end
 
 % construct PD controller
-Kp = 170;
-Kd = 2*sqrt(Kp);
-cv = 1;
-cd = 5;
+% Kp = 170;
+% Kd = 2*sqrt(Kp);
+% cv = 0.1;
+% cd = 0;
 % c = PDController(r,Kp,Kd,cv,cd);
-c = PDController(r,Kp,Kd,cv,cd,xtraj1,utraj1);
+c = PDController(r,Kp,Kd,cv,cd,xtraj,utraj);
 
 sys = feedback(r,c);
 
 % Run simulation, then play it back at realtime speed
 xtraj = simulate(sys,[0 3],double(x0));
-% playback(v,xtraj,struct('slider',true));
 
-% Find controller outputs corresonding to each time step
+% playback(v,xtraj,struct('slider',true)); % commented out for speed
+
+% % Find controller outputs corresonding to each time step - used to generate
+% % utraj for warm starting RL
+% current_target_state = 2;
 % u = zeros(4,length(xtraj.tt));
 % for i = 1:length(xtraj.tt)
 %     x = xtraj.xx(:,i);
 %     u(:,i) = c.output(xtraj.tt(i),0,x);
 % end
 
-zf = xtraj.xx(2,end);
-% end
-toc
+% zf = xtraj.xx(2,end); % for debugging
+
+end
